18f82c023cb89fbe60b5b84447430884422821c4
Gabriel Henrique <gabrielhenriqueferreiraalves07@gmail.com>
2024-02-21 19:45:17 -0400

    Att.21.02.2024

---
+++ Aula 20.02.2024/exercicio03.js
@@ -0,0 +1,5 @@
+const number = 10;
+
+if (numbers === 1) {
+    console.log(numbers = number * 0.10);
+}
---
+++ Aula 20.02.2024/exercicio04.js
@@ -0,0 +1,8 @@
+function soma(number) {
+    let number = 0;
+
+    for (let i = 0; i < number.length; i++) {
+        array = number[i];
+    }
+    return array;
+}
